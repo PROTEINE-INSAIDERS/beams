604bf0d653c03177a2e0ab1d284809ebe2eb5ade
Черничкин Станислав Викторович <Chernichkin.S.Vi@sberbank.ru>
2019-08-11 22:59:11 +0300

    cleanup

--- src/main/scala/beams/akka/AkkaBeam.scala
+++ src/main/scala/beams/akka/AkkaBeam.scala
@@ -6,7 +6,6 @@ import _root_.akka.util._
 import beams._
 import scalaz.zio._
 
-// контекст передавать нельзя, он не потокобезопасен!
 final case class AkkaBeam[+Env](self: AkkaNode[Env], env: Env, timeout: LocalTimeout, scheduler: Scheduler) extends Beam[AkkaNode, Env] {
   override val beam: Beam.Service[AkkaNode, Env] = AkkaBeam.Service[Env](self, env, timeout, scheduler)
 }
@@ -20,21 +19,20 @@ object AkkaBeam {
                                   scheduler: Scheduler
                                 ) extends Beam.Service[AkkaNode, Env] {
     override def forkTo[R, A](node: AkkaNode[R])
-                             (task: TaskR[Beam[AkkaNode, R], A]): Task[Fiber[Throwable, A]] = Task.fromFuture { implicit ctx =>
+                             (task: TaskR[Beam[AkkaNode, R], A]): Task[Fiber[Throwable, A]] = Task.fromFuture { _ =>
       implicit val t: Timeout = timeout.current()
       implicit val s: Scheduler = scheduler
 
       node.ref
         .ask[Exit[Throwable, Any]](NodeActor.RunTask(task.asInstanceOf[TaskR[Beam[AkkaNode, Any], Any]], _, timeout.migratable()))
-    }
-      .flatMap(exit => Task.done(exit.asInstanceOf[Exit[Throwable, A]]))
+    }.flatMap(exit => Task.done(exit.asInstanceOf[Exit[Throwable, A]]))
       .fork
 
-    override def createNode[R](a: R): Task[AkkaNode[R]] = Task.fromFuture { implicit ctx =>
+    override def createNode[R](a: R): Task[AkkaNode[R]] = Task.fromFuture { _ =>
       implicit val t: Timeout = timeout.current()
       implicit val s: Scheduler = scheduler
-      self.ref.ask[NodeActor.Ref](NodeActor.CreateNode(a, _)).map(AkkaNode[R])
-    }
+      self.ref.ask[NodeActor.Ref](NodeActor.CreateNode(a, _))
+    }.map(AkkaNode[R])
 
     override def releaseNode[R](node: AkkaNode[R]): Task[Unit] = Task(node.ref.tell(NodeActor.Stop))
   }
