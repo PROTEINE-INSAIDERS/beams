d8c50e0a2761b91a31e65fcd2bf90124c0c57155
Черничкин Станислав Викторович <Chernichkin.S.Vi@sberbank.ru>
2019-08-14 17:57:27 +0300

    WIP: #4, #5

--- beams/src/main/scala/beams/akka/local/package.scala
+++ beams/src/main/scala/beams/akka/local/package.scala
@@ -1,9 +1,27 @@
 package beams.akka
 
 import akka.actor.typed._
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
+import beams.Beam
+import scalaz.zio._
 
 package object local {
   def createActorSystem(name: String): ActorSystem[SpawnProtocol.Command] = {
     ActorSystem(SpawnProtocol(), name)
+  }
+
+  def beam[A](task: TaskR[Beam[AkkaNode, Any], A], system: ActorSystem[SpawnProtocol.Command], timeout: LocalTimeout): Task[A] = {
+    Managed.make(IO.fromFuture { implicit ctx =>
+      implicit val ct: Timeout = timeout.current()
+      implicit val sch: Scheduler = system.scheduler
+      system.ask[NodeActor.Ref](SpawnProtocol.Spawn(NodeActor(()), "", Props.empty, _))
+    })(r => ZIO.effectTotal(r.tell(NodeActor.Stop))).use { root =>
+      IO.fromFuture { implicit ctx =>
+        implicit val ct: Timeout = timeout.current()
+        implicit val sch: Scheduler = system.scheduler
+        root.ask[Exit[Throwable, Any]](replyTo => NodeActor.RunTask(task, replyTo, timeout.migratable()))
+      }
+    }.flatMap(exit => IO.done(exit.asInstanceOf[Exit[Throwable, A]]))
   }
 }
--- examples/map-reduce/src/main/scala/Main.scala
+++ examples/map-reduce/src/main/scala/Main.scala
@@ -2,61 +2,81 @@ import beams._
 import scalaz.zio._
 
 import scala.collection._
-import scala.reflect.ClassTag
 
 object Main {
 
   trait Source[K, V] {
-    def keys: Iterable[K]
+    def numPartitions: Int
 
-    def values(k: K): Iterable[V]
+    def partition(n: Int): Iterable[(K, V)]
   }
 
   private def shrunk(hash: Int, bound: Int): Int = {
     (java.lang.Integer.toUnsignedLong(hash) * bound >>> 32).toInt
   }
 
+  private def addReduced[K, V](data: mutable.Map[K, V], reduce: (V, V) => V, items: Iterable[(K, V)]): mutable.Map[K, V] = {
+    for ((key, value) <- items) {
+      if (data.contains(key)) {
+        data(key) = reduce(data(key), value)
+      } else
+        data(key) = value
+    }
+    data
+  }
+
   def mapReduce[N[+ _], K1, V1, K2, V2](
                                          source: Source[K1, V1],
                                          map: (K1, V1) => Iterable[(K2, V2)],
                                          reduce: (V2, V2) => V2
-                                       )
-                                       (implicit e1: ClassTag[N[Int]]
-                                        // ,e2: ClassTag[N[mutable.Map[K, V]]]
-                                       ): ZIO[Beam[N, Any], Throwable, Unit] = {
+                                       ): ZIO[Beam[N, Any], Throwable, Iterable[(K2, V2)]] = {
     val syntax = BeamSyntax[N]()
     import syntax._
-    val capacity = java.lang.Runtime.getRuntime.availableProcessors()
+    val capacity = math.min(java.lang.Runtime.getRuntime.availableProcessors(), source.numPartitions)
     for {
-      mappers <- ZIO.foreach(source.keys)(node)
+      mappers <- ZIO.foreach(0 until capacity)(node)
       reducers <- ZIO.foreach(0 until capacity)(_ => Ref.make(mutable.Map.empty[K2, V2]) >>= node)
-      result <- (Managed.collectAll(mappers) <*> Managed.collectAll(reducers)).use { case (mappers, reducerList) => for {
-        reducers <- ZIO(reducerList.toArray)
-        aaaa <- ZIO.foreachPar(mappers) { mapper => ??? }
-      }  yield ???
-      /*
-      reducers <- ZIO.foreach(0 until capacity)(_ => node(mutable.Map.empty[K, V]))
-      result <- (Managed.collectAll(mappers) <*> Managed.collectAll(reducers)).use { case (mapperList, reducerList) => for {
-        mappers <- ZIO(mapperList.toArray)
-        reducers <- ZIO(reducerList.toArray)
-        aaaa <- ZIO.foreach(mappers) { mapper =>
-          forkTo(mapper) {
-            for {
-              partition <- env[Int]
-              data <- ZIO(map(source.partition(partition)))
-              _ <- ZIO.foreach(data) { case (key, value) => for {
-                _ <- forkTo(???) {
-                  ???
+      result <- (Managed.collectAll(mappers) <*> Managed.collectAll(reducers)).use {
+        case (mappers, reducerList) => for {
+          reducers <- ZIO.succeed(reducerList.toVector)
+          mapperFibers <- ZIO.foreach(mappers) { mapper =>
+            forkTo(mapper) {
+              for {
+                partitionNo <- env[Int]
+                partitionedData <- IO {
+                  addReduced(new mutable.HashMap[K2, V2], reduce, source.partition(partitionNo).flatMap { case (k, v) => map(k, v) })
+                    .groupBy { case (k, _) => shrunk(k.hashCode(), capacity) }
                 }
+                reducerFibers <- ZIO.foreachPar(partitionedData) {
+                  case (partition, mappedData) => forkTo(reducers(partition)) {
+                    for {
+                      ref <- env[Ref[mutable.Map[K2, V2]]]
+                      _ <- ref.update { reducedData =>
+                        addReduced(reducedData, reduce, mappedData)
+                      }
+                    } yield ()
+                  }
+                }
+                _ <- Fiber.joinAll(reducerFibers)
               } yield ()
-              }
-            } yield ???
+            }
           }
-        }
-      } yield ()
+          _ <- Fiber.joinAll(mapperFibers)
+          collectFibers <- ZIO.foreach(0 until capacity) { partNo =>
+            forkTo(reducers(partNo))(env[Ref[mutable.Map[K2, V2]]].flatMap(_.get))
+          }
+          result <- collectFibers.foldLeft(IO(new mutable.ArrayBuffer[(K2, V2)])) { (io, f) =>
+            for {
+              collectorData <- io
+              reducerData <- f.join
+            } yield {
+              collectorData.appendAll(reducerData)
+              collectorData
+            }
+          }
+        } yield result
       }
-       */
-    } yield ???
+    } yield result
   }
 
   def main(args: Array[String]): Unit = {
