e3cc780248ec329ffa8ea3d733fbad70dad9901a
Черничкин Станислав Викторович <Chernichkin.S.Vi@sberbank.ru>
2019-08-16 00:25:21 +0300

    WIP: #15

--- examples/cluster/src/main/scala/Main.scala
+++ examples/cluster/src/main/scala/Main.scala
@@ -3,21 +3,15 @@ import akka.actor.typed.scaladsl._
 import akka.cluster.ClusterEvent._
 import akka.cluster.MemberStatus
 import akka.cluster.typed._
+import akka.actor.typed.scaladsl.adapter._
+
 
 object Main {
   def main(args: Array[String]): Unit = {
-
-    val system1 = beams.akka.local.createActorSystem("system1")
-
-    val cluster1 = Cluster(system1)
-
-    cluster1.manager ! Join(cluster1.selfMember.address)
-
-    // примерная схема работы кластера:
-    // 1. поднимается система с NodeActor.
-    // 2. поднимается менеджер, следящий за кластером.
-    // 3. менеджер содержит список нод, которые могут быть использованы для запуска программ.   
-
-    cluster1.manager ! Leave(cluster1.selfMember.address)
+    val untypedSystem = akka.actor.ActorSystem("ClusterSystem")
+    val system = untypedSystem.toTyped
+    val cluster = Cluster(system)
+    scala.io.StdIn.readLine()
+    ()
   }
 }
